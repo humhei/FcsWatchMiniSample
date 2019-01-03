b28a95e28656a2beb5ea64fc4360caaf65e65d8a
humhei <humhei@outlook.com>
2019-01-03 21:27:28 +0800

    Initial

---
+++ build.fsx
@@ -0,0 +1,25 @@
+#r "paket:
+nuget Atrous.Core.Utils
+nuget Fake.Core.Target
+nuget FcsWatch //"
+
+#load "./.fake/build.fsx/intellisense.fsx"
+
+// start build
+open Fake.Core
+open Fake.IO
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open FcsWatch.FakeHelper
+open Atrous.Core.Utils.FakeHelper
+
+let root = Path.getDirectory "./"
+Target.create "FcsWatch" (fun _ ->  
+    let projectFile = Path.getFullName "FcsWatchMiniSample.fsproj"
+    dotnet root "build" ["projectFile"]
+    let checker = FSharpChecker.Create()
+    runFcsWatcher checker projectFile
+)
+
+Target.create "Default" ignore
+
+Target.runOrDefault "Default"
